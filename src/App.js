8a84718622d01c42323ee570902302950a06f4ba
Victor <victor@redradix.com>
2021-01-25 18:26:44 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react'
+import './App.css'
+import placeholderImg from './placeholder.png'
+import { ReactComponent as ChevronLeft } from './chevron-left.svg'
+import { ReactComponent as ChevronRight } from './chevron-right.svg'
 
 function App() {
+  const [searchResult, setSearchResult] = useState()
+
+  useEffect(() => {
+    const search = async () => {
+      const response = await fetch(
+        'http://www.omdbapi.com/?apikey=a461e386&s=king',
+      )
+
+      const data = await response.json()
+
+      if (!searchResult) {
+        setSearchResult(data)
+      }
+    }
+
+    search()
+  })
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="search">
+        <input type="text" placeholder="Search..." />
+        <button>Search</button>
+      </div>
+      {!searchResult ? (
+        <p>No results yet</p>
+      ) : (
+        <div className="search-results">
+          <div className="chevron">
+            <ChevronLeft />
+          </div>
+          <div className="search-results-list">
+            {searchResult.Search.map(result => (
+              <div key={result.imdbID} className="search-item">
+                <img
+                  src={result.Poster === 'N/A' ? placeholderImg : result.Poster}
+                  alt="poster"
+                />
+                <div className="search-item-data">
+                  <div className="title">{result.Title}</div>
+                  <div className="meta">{`${result.Type} | ${result.Year}`}</div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div className="chevron">
+            <ChevronRight />
+          </div>
+        </div>
+      )}
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
