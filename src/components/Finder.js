54e63b2cf330bc95cb889961c5ca3b8fff2ffa8a
vtendero <tendero.valle@gmail.com>
2021-04-07 00:27:49 +0200

    Add finder component

---
+++ src/components/Finder.js
@@ -0,0 +1,30 @@
+const Finder = props => {
+  const handleChange = ev => {
+    console.log(ev)
+    props.handleInput({
+      value: ev.target.value,
+    })
+  }
+
+  const handleOnSubmit = ev => {
+    ev.preventDefault()
+  }
+
+  const handleOnClick = () => {
+    props.handleSearch()
+  }
+
+  return (
+    <form className="search" onSubmit={handleOnSubmit}>
+      <input
+        type="text"
+        placeholder="Search..."
+        value={props.searchInput}
+        onChange={handleChange}
+      />
+      <input type="button" value="Search" onClick={handleOnClick} />
+    </form>
+  )
+}
+
+export default Finder
