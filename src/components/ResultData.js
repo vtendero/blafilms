0506eab3822d02e9d6a5d26e2932b58f827761f6
vtendero <tendero.valle@gmail.com>
2021-04-06 16:59:14 +0200

    Add result data component

---
+++ src/components/ResultData.js
@@ -0,0 +1,18 @@
+import placeholderImg from '../images/placeholder.png'
+
+const ResultData = props => {
+  return (
+    <>
+      <img
+        src={
+          props.result.Poster === 'N/A' ? placeholderImg : props.result.Poster
+        }
+        alt={`Poster de ${props.result.Title}`}
+      />
+      <h5 className="title">{props.result.Title}</h5>
+      <p className="meta">{`${props.result.Type} | ${props.result.Year}`}</p>
+    </>
+  )
+}
+
+export default ResultData
