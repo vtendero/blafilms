76188ba148565c61dc1aa09162eceb7f6ca45610
vtendero <tendero.valle@gmail.com>
2021-04-08 01:52:04 +0200

    Add next page and previous page components

---
+++ src/components/NextPage.js
@@ -0,0 +1,11 @@
+import { ReactComponent as ChevronRight } from '../images/chevron-right.svg'
+
+const NextPage = () => {
+  return (
+    <div className="chevron">
+      <ChevronRight />
+    </div>
+  )
+}
+
+export default NextPage
---
+++ src/components/PreviousPage.js
@@ -0,0 +1,11 @@
+import { ReactComponent as ChevronLeft } from '../images/chevron-left.svg'
+
+const PreviousPage = () => {
+  return (
+    <div className="chevron">
+      <ChevronLeft />
+    </div>
+  )
+}
+
+export default PreviousPage
