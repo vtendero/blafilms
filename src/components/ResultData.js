c7c057bb2fbe816529dc8957223ca34ce9e50937
vtendero <tendero.valle@gmail.com>
2021-04-08 01:16:11 +0200

    Correct name to alt attribute

--- src/components/ResultData.js
+++ src/components/ResultData.js
@@ -7,7 +7,7 @@ const ResultData = props => {
         src={
           props.result.Poster === 'N/A' ? placeholderImg : props.result.Poster
         }
-        alt={`Poster de ${props.result.Title}`}
+        alt={`${props.result.Title} poster`}
       />
       <h5 className="title">{props.result.Title}</h5>
       <p className="meta">{`${props.result.Type} | ${props.result.Year}`}</p>
